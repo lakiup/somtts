b2926dbdfc8d0ef2ab5b85123f8b3a573d8d0d77
Lɑƙɪ <lakiup2@gmail.com>
2026-02-21 20:30:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ app = Client(
 )
 
 YDL_OPTS_YOUTUBE = {
-    'format': 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
+    'format': 'best',
     'outtmpl': 'video_%(id)s.%(ext)s',
     'quiet': True,
     'no_warnings': True,
